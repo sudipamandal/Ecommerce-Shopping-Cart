ef8d8979e45e6357797f13a2b6ff48e8213e3ca5
Sudipa Mandal <104677740+sudipamandal@users.noreply.github.com>
2023-06-17 11:37:16 +0530

    Create Product.js

---
+++ src/pages/Product.js
@@ -0,0 +1,58 @@
+const Products = [
+  {
+    id: 1,
+    productName: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109.5,
+    productImage: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  },
+  {
+    id: 2,
+    productName: "Mens Casual Premium Slim Fit T-Shirts",
+    price: 22.3,
+    productImage:
+      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+  },
+  {
+    id: 3,
+    productName: "Mens Cotton Jacket",
+    price: 55.99,
+    productImage: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+  },
+  {
+    id: 4,
+    productName: "Mens Casual Slim Fit",
+    price: 15.99,
+    productImage: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+  },
+  {
+    id: 5,
+    productName:
+      "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+    price: 695,
+    productImage:
+      "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+  },
+  {
+    id: 6,
+    productName: "Solid Gold Petite Micropave",
+    price: 168,
+    productImage:
+      "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+  },
+  {
+    id: 7,
+    productName: "White Gold Plated Princess",
+    price: 9.99,
+    productImage:
+      "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
+  },
+  {
+    id: 8,
+    productName: "Pierced Owl Rose Gold Plated Stainless Steel Double",
+    price: 10.99,
+    productImage:
+      "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
+  },
+];
+
+export default Products;
